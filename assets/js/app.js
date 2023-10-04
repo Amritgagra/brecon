6515509ad83092bbd512b05b181b57bc9117c89f
Amrit <amritgagra200@gmail.com>
2023-10-04 17:21:50 +0530

    fix navigation issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,3 @@
-$( ".job__item" ).hover(
-  function() {
-    $('.job__item').addClass('active')
-    $( this ).removeClass( "active" );
-  }, function() {
-    $('.job__item').removeClass('active')
-    $('.job__item').removeClass('active')
-  }
-);
-
 // const banner = document.querySelector('.banner');
 // const header =  document.querySelector('.header')
 // window.onscroll = function() {scrollFunction(banner.offsetHeight)};
@@ -20,10 +10,22 @@ $( ".job__item" ).hover(
 // }
 
 $(document).ready(function() {
-  if($(window).width() < 993) {
-    window.onscroll = function() {scrollFunction(0)};
-  }
+  // if($(window).width() < 993) {
+  //   window.onscroll = function() {scrollFunction(0)};
+  // }
+  $('.nav-link:not(.lang-link)').on('click',function(){
+    if($(this).hasClass('show')){
+      $('.banner .topbar').addClass('position-fixed');
+      $('.banner .topbar').css('top',`${$('.header').outerHeight()}px`);
+    }
+    else {
+      $('.banner .topbar').removeClass('position-fixed');
+      $('.banner .topbar').css('top','0px');
+    }
+  })
 });
+
+
 
 
 if (typeof Swiper !== 'undefined') {
